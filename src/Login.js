418b3d775fb53f8daa470cc597581a16e465bf14
Mayank Upadhyay <mayank6220@gmail.com>
2024-09-02 17:54:47 +0530

    change in colour,identation

--- src/Login.js
+++ src/Login.js
@@ -1,28 +1,31 @@
-// src/Login.js
 import React from "react";
 import "./Login.css";
+import googleImg from "./assets/google.png";
+import facebookImg from "./assets/facebook.png";
+import appleImg from "./assets/apple.png";
+import logoImg from "./assets/IYUREK.png";
 
 const Login = () => {
     return (
         <div className="login-container">
             <div className="login-box">
                 <div className="login-header">
-                    <img src="../component/IYUREK.png" alt="Logo" className="logo" />
+                    <img src={logoImg} alt="Logo" className="logo" />
                     <h2>Sign In</h2>
                 </div>
                 <div className="social-login">
-                    <button className="social-button google">Google</button>
-                    <button className="social-button facebook">Facebook</button>
-                    <button className="social-button apple">Apple</button>
+                    <img src={googleImg} alt="Google Login" className="social-button google" />
+                    <img src={facebookImg} alt="Facebook Login" className="social-button facebook" />
+                    <img src={appleImg} alt="Apple Login" className="social-button apple" />
                 </div>
                 <form>
                     <div className="input-group">
                         <label>Email</label>
-                        <input type="email" placeholder="Enter your email" />
+                        <input type="email" placeholder="" />
                     </div>
                     <div className="input-group">
                         <label>Password</label>
-                        <input type="password" placeholder="Enter your password" />
+                        <input type="password" placeholder="" />
                     </div>
                     <a href="#" className="forgot-password">Forgot Password?</a>
                     <button type="submit" className="login-button">Sign In</button>
@@ -35,9 +38,9 @@ const Login = () => {
                 </div>
                 <footer>
                     <p>2024 © Iyurek</p>
-                    <p>A product of Wall Street Business
-                        Academy Holdings, Inc.All Rights Reserved</p>
-
+                    <p>A product of Wall Street Business Academy Holdings, Inc.
+                        <br />
+                        All Rights Reserved</p>
                 </footer>
             </div>
             <div className="close-button">✕</div>
